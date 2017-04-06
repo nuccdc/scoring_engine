9b4252eef6f15ada70dd4f8862a3175eba048d4c
Alex Interrante-Grant <a.interrantegrant@gmail.com>
2017-04-06 12:37:49 -0400

    fix a race condition in rabbitmq container startup

--- queue/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-( sleep 5
-rabbitmqctl add_user scoring_engine scoring_engine 2> /dev/null
-rabbitmqctl set_permissions scoring_engine '.*' '.*' '.*' 2> /dev/null ) &
-
-rabbitmq-server $@
